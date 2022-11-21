f8f6d3157b2b7897d79a4c53bafe07b948e38c96
Isa Nazhmudinov <i.nazhmudinov@corp.mail.ru>
2022-11-21 22:55:38 +0400

    add: redis flag

--- flags/redis_flags.go
+++ flags/redis_flags.go
@@ -6,8 +6,8 @@ import (
 
 type RedisFlags struct {
 	Host     string `kong:"required,group=Redis,name=redis-host,env=REDIS_HOST,help=localhost:6379"`
+	Username string `kong:"optional,group=Redis,name=redis-username,env=REDIS_USERNAME,default=''"`
 	Password string `kong:"optional,group=Redis,name=redis-password,env=REDIS_PASSWORD,default=''"`
-	Username string `kong:"optional,group=Redis,name=redis-password,env=REDIS_PASSWORD,default=''"`
 }
 
 func (f RedisFlags) Init() *redis.Client {
